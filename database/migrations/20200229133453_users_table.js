1f1b4d219aa9b1430b0fc6d5f81f3d001131dfa8
CorellCB <corellcb@gmail.com>
2020-03-03 17:15:41 -0500

    added first name and last name to users

--- database/migrations/20200229133453_users_table.js
+++ database/migrations/20200229133453_users_table.js
@@ -7,7 +7,12 @@ exports.up = function(knex) {
         .string('username', 128)
         .notNullable()
         .unique();
+
       users.string('password', 128).notNullable();
+
+      users.string('first_name', 128).notNullable();
+
+      users.string('last_name', 128).notNullable();
   });
 };
 
--- database/seeds/users.js
+++ database/seeds/users.js
@@ -6,9 +6,11 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex("users").insert([
         {
+          first_name: "Jo",
+          last_name: "Doe",
           username: "test",
           password:
-            "$2y$08$BsnCyZtPXjL4ROy6ygYEGOHMBr1x4yOgED.MU3pOWfKimQg.Jhqcy"
+            "$2y$08$BsnCyZtPXjL4ROy6ygYEGOHMBr1x4yOgED.MU3pOWfKimQg.Jhqcy",
         }
       ]);
     });
